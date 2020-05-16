855648ee3843ba9e0e9b0afb60983806d9e0f4a8
Oscar Pérez <mr.omiguelperez@gmail.com>
2020-05-16 10:09:57 -0500

    Kata 3 - BOT

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,27 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    result = 'Hola, Geeks!'
+    update.message.reply_text(result)
+    return result
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    result = 'Ayudame!'
+    update.message.reply_text(result)
+    return result
 
 def mayus(update, context):
-        #
-        return ""
+    result = context.args[0].upper()
+    update.message.reply_text(result)
+    return result
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    #
-    return ""
+    text = update.message.text
+    result = text[::-1]
+    update.message.reply_text(result)
+    return result
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -37,15 +44,15 @@ def main():
     updater = Updater("", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_error_handler(CommandHandler('start', start))
+    dp.add_error_handler(CommandHandler('help', help))
+    dp.add_error_handler(CommandHandler('mayus', mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    dp.add_error_handler(CommandHandler('alreves', alreves))
     
     # Y este espera al error
     dp.add_error_handler(error)
